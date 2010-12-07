ff16e23fa4949f698e953573eab9147b2115f6bc
Jon Renaut <jon@manfredmacx.com>
2010-12-06 22:25:36 -0500

    Working initial commit

---
+++ ask.py
@@ -0,0 +1,87 @@
+import web
+from web import form
+
+render = web.template.render('templates/')
+
+urls = ('/', 'index')
+
+EASTER_EGGS = ["seahorse", "seahorses",] 
+IDIOTS = ["JessicaSimpson", "ParisHilton", "lindsaylohan", "kanyewest", "glennbeck", "GovMikeHuckabee", "JoeLieberman",
+	"katyperry", "algore", "KarlRove", "TilaTequila", "SarahPalinUSA"]
+		
+app = web.application(urls, globals())
+
+
+myform = form.Form( 
+	form.Textbox("term"),) 
+
+class index:
+	def GET(self): 
+		form = myform()
+		return render.formtest(form)
+
+	def POST(self): 
+		form = myform() 
+		if not form.validates(): 
+			return render.formtest(form)
+		else:
+			term = form['term'].value
+			if term in EASTER_EGGS:
+				return self.handleEasterEgg(term)
+			return self.handleSearch(term)
+
+	def handleSearch(self, term):
+		import twitter
+		import random
+		api = twitter.Api()
+		statuses = []
+		for i in random.sample(IDIOTS, 3):
+			tmp = api.GetUserTimeline(i, count=50)
+			if tmp is not None:
+				statuses = statuses + tmp
+		def _compare(x, y):
+			if y is None and x is None:
+				return None
+			elif y is None:
+				intersection = filter(lambda l:l in x.text.rsplit(" "), _getList(term))
+				if len(intersection) > 0:
+					return x
+				return None
+			elif x is None:
+				intersection = filter(lambda l:l in y.text.rsplit(" "), _getList(term))
+				if len(intersection) > 0:
+					return y
+				return None
+			else:
+				terms = _getList(term)
+				intersectionx = filter(lambda l:l in x.text.rsplit(" "), terms)
+				intersectiony = filter(lambda l:l in y.text.rsplit(" "), terms)
+				sizeDiff = len(intersectionx) - len(intersectiony)
+				if sizeDiff > 0:
+					return x
+				elif sizeDiff < 0:
+					return y
+				else:
+					if len(intersectionx) == len(intersectiony) and len(intersectionx) == 0:
+						return None
+					elif x.created_at > y.created_at:
+						return x
+					else:
+						return y
+			return None
+
+		def _getList(term):
+			from thesaurus import thesaurus
+			t = thesaurus()
+			return t.getAll(term)
+		result = reduce(_compare, statuses)
+		if result is None:
+			return render.howiroll(myform())
+		return render.result(result, term, myform())
+			
+	def handleEasterEgg(self, term):
+		if term == "seahorse" or term == "seahorses":
+			return render.cups(myform())
+
+if __name__ == "__main__":
+	app.run()
---
+++ thesaurus.py
@@ -0,0 +1,31 @@
+import simplejson
+import urllib
+from settings import THESAURUS_API_KEY as API_KEY
+
+class thesaurus:
+	def search(self, term):
+		url = "http://words.bighugelabs.com/api/2/{0}/{1}/json".format(API_KEY, term,)
+		result = simplejson.load(urllib.urlopen(url))
+		return result
+	def get(self, term, partOfSpeech="noun", resultType="ant"):
+		result = self.search(term)
+		try:
+			pos = result.get(partOfSpeech)
+			pos = pos.get(resultType)
+			return pos
+		except:
+			return None
+	def getAll(self, term):
+		result = self.search(term)
+		retval = []
+		try:
+			for a, b in result.items():
+				if type(b) == "<type 'list'>":
+					retval = retval + b
+				else:
+					for c, d in b.items():
+						retval = retval + d
+			return retval	
+		except:
+			return [term]
+	
